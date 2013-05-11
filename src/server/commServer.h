14a536a7f49751b0f26bacd2bf14cb3354f16432
Andrew Thompson <andrewthompson10@gmail.com>
2013-05-11 14:38:53 -0700

    Finished writing the API for commServer

--- src/server/commServer.h
+++ src/server/commServer.h
@@ -1,13 +1,48 @@
 #ifndef COMM_SERVER_H
-#define COMM_SERVER_H
+
+#include "notrap.h"
 
 typedef struct COMM_server_struct COMM_server;
+typedef struct COMM_connection_struct COMM_connection;
 
-messageReceivedCallback();
 
-int COMM_sendMessage();
+//----------------------------------------------------------------------------
+// Callbacks that get called when responses are receieved, or when
+// a new connection comes.
+//
+// We place these first because they are used when starting the library,
+// which is the next section.
+//---------------------------------------------------------------------------
 
-COMM_server *COMM_initializeServer( newConnectionCallback );
+void (*COMM_ResponseReceived_cb)(COMM_connection*client, 
+                                 int code, BOOL result,
+                                 const char *response1,
+                                 const char *response2,
+                                 const char *response3,
+                                const char *response4,
+                                 const char *errorMessage,
+                                 void *context);
+
+void (*COMM_binaryResponseReceived_cb)(COMM_connect*client, 
+                                       int code, BOOL result,
+                                       const char*response1,
+                                       const char*binaryResponse,
+                                       int binaryResponseSize,
+                                       char *errorMessageOut,
+                                       void *context);
+
+BOOL (*COMM_connectionStatusCallback)(COMM_connection *connection, int STATUS);
+
+
+
+//---------------------------------------------------------------------------
+// Functions for starting, stopping, and running the COMM server
+//---------------------------------------------------------------------------
+
+COMM_server *COMM_initServer(COMM_connectionStatusCallback   *css_cb,
+                             void *connectionStatusContext,
+                             COMM_messageResponseReceived_cb *mrr_cb,
+                             COMM_binaryResponseReceived_cb  *bmrr_cb);
 
 int COMM_beginListening(int port);
 
@@ -15,4 +50,21 @@ int COMM_runServerLoop(COMM_server *obj);
 
 int COMM_shutdownServer(COMM_server **obj);
 
+//----------------------------------------------------------------------------
+// Functions for sending messages
+//----------------------------------------------------------------------------
+int COMM_sendMessage(COMM_connection*client, int code,
+                     const char *param1,
+                     const char *param2,
+                     const char *param3,
+                     const char *param4, void *context);
+
+int COMM_sendBinaryMessage(COMM_connection*client, int code,
+                           const char *param1,
+                           const char *binaryPayload,
+                           int binarySize,
+                           void *context);
+
+
 #endif
+
