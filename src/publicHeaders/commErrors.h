30c235ff158141434dc11f81b8eb6c45c2c9a664
Andrew Thompson <andrewthompson10@gmail.com>
2013-05-15 20:26:23 -0700

    Added a file for erros

---
+++ src/publicHeaders/commErrors.h
@@ -0,0 +1,27 @@
+#ifndef COMM_ERRORS_H
+#define COMM_ERRORS_H
+
+/* This file doesn't need to be included directly, it is included
+ * from commServer.h and commClient.h.
+ *
+ * It lists all the errors that can be returned from COMM functions.
+ * Useful to have them all in one place, since many are shared by both
+ * server and client.
+ * 
+ * Copyright 2013 Andrew Usable under the terms of the GPL 3.0 or greater
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
