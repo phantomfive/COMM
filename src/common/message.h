37520e9158260cca9e42d7019f0f4f39cf8be659
Andrew Thompson <andrewthompson10@gmail.com>
2013-06-17 08:57:57 -0700

    Added a new comment describing the message format because I'm changing the way the message is created

--- src/common/message.h
+++ src/common/message.h
@@ -41,13 +41,107 @@ struct Message {
 #define USAGE_INTERNAL 0x1
 #define USAGE_USER     0x2
 
+
+//-------------------------------------------------------------------------
+// Message Types: Protocol definition 
+//-------------------------------------------------------------------------
+
+/*
+TYPE_STRING format
+
++-------+---------------------------------------------------------+
+| bytes | description                                             |
++-------+---------------------------------------------------------+
+| 0-3   | uint32_t total message size (including these four bytes)|
+| 4-7   | uint32_t type: one of TYPE_*                            |
+| 8-11  | uint32_t usage: one of USAGE_*                          |
+| 12-15 | uint32_t correlationId                                  |
+| 16-19 | uint32_t code that is passed to the user                |
+| 20-23 | uint32_t size of string param 1, including null byte    |
+| 24-27 | uint32_t size of string param 2                         |
+| 28-31 | uint32_t size of string param 3                         |
+| 32-35 | uint32_t size of string param 4                         |
+| 36-39 | uint32_t size of error message (strings can be zero len)|
+| 40    | uint8_t  true on error, 0 if not error                  |
+|       |                                                         |
+| 41-   |   +-------------------------------------------------+   |
+|       |   | String param 1                                  |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   +-------------------------------------------------+   |
+|       |   | String param 2                                  |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   +-------------------------------------------------+   |
+|       |   | String param 3                                  |   |
+|       |   | (can be length 1, if there is no string, but    |   |
+|       |   |  still needs a null byte)                       |   |
+|       |   +-------------------------------------------------+   |
+|       |   | String param 4                                  |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   +-------------------------------------------------+   |
+|       |   | Error string                                    |   |
+|       |   +-------------------------------------------------+   |
++-------+---------------------------------------------------------+
+
+TYPE_BINARY format
+
++-------+---------------------------------------------------------+
+| bytes | description                                             |
++-------+---------------------------------------------------------+
+| 0-3   | uint32_t total message size (including these four bytes)|
+| 4-7   | uint32_t type: one of TYPE_*                            |
+| 8-11  | uint32_t usage: one of USAGE_*                          |
+| 12-15 | uint32_t correlationId                                  |
+| 16-19 | uint32_t code that is passed to the user                |
+| 20-23 | uint32_t size of the string parameter                   |
+| 24-27 | uint32_t size of the binary parameter                   |
+| 28-31 | uint32_t size of the error message                      |
+| 32    | uint8_t true on error, false if no error                |
+| 33-   |   +-------------------------------------------------+   | 
+|       |   | String param                                    |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   +-------------------------------------------------+   |
+|       |   | Binary param                                    |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
+|       |   +-------------------------------------------------+   |
+|       |   | Error string                                    |   |
+|       |   |                                                 |   |
+|       |   |                                                 |   |
++-------+---+-------------------------------------------------+---+
+
+*/
+
+
+
+
 /* StringMessage, inherits from Message */
 struct StringMessage {
 	//used locally, not sent to the other side
 	const void *context;
 
 	//Fields that get sent across the wire
-	uint32_t type;                //same as parent class
+	/*uint32_t type;                //same as parent class
 	uint32_t usage;               //one of the USAGE_* constants
 	uint32_t correlationId;       //The message and its response share this id
 	uint32_t code;                //This and the rest are all set by the user
@@ -56,7 +150,7 @@ struct StringMessage {
 	uint32_t param3Size;
 	uint32_t param4Size;
 	uint32_t errMsgSize;
-	uint8_t  isError;
+	uint8_t  isError;*/
 	uint8_t  params[0];   //allocated at runtime to hold all the params
 };
 
