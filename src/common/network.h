fffe62b54e3a19c29013c5d80b7051a03bb589aa
Andrew Thompson <andrewthompson10@gmail.com>
2013-06-19 09:44:00 -0700

    First attempt at a network API

---
+++ src/common/network.h
@@ -0,0 +1,38 @@
+#ifndef COMM_NETWORK_H
+#define COMM_NETWORK_H
+/***************************************************************************
+ * Class for sending and receiving data over a network: non-blocking.
+ *
+ * Copyright Andrew 2013 Usable under the terms of the GPL 3.0 or greater
+ ***************************************************************************/
+#include <notrap.h>
+
+struct COMMnet;
+
+/*Only fails if there is no memory*/
+COMMnet *COMMinitNetwork();
+void COMMshutdownNetwork(COMMnet **net);
+
+/*Runs the network for one loop*/
+COMMStatus COMMrunNetwork(COMMnet *net);
+
+void COMMnetConnect(COMMnet *net, const char *dest, uint16_t port);
+
+void COMMnetListen(COMMnet *net, uint16_t port);
+
+void COMMnetSendData(COMMnet *net, const uint8_t*data, uint32_t len,
+                     int timeoutSeconds, COMMnetSent_cb*cb, void *context);
+
+void COMMnetRecvData(COMMnet *net, const uint8_t*data, uint32_t len,
+                     int timeoutSeconds, COMMnetRecvd_cb*cb, void*context);
+
+
+
+
+
+
+
+
+
+#endif
+
